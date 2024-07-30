cac4577ee7c681a63ec599604758b4e4db3bdeec
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-07-30 20:44:29 -0300

    feat: add resources SSH key management and project organization

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,18 @@
+resource "digitalocean_ssh_key" "sige_ssh_key" {
+  name       = "sige-ssh-key"
+  public_key = var.ssh_key
+}
+
 resource "digitalocean_droplet" "api_homolog" {
-  name   = "sige-api-homolog"
-  image  = "debian-11-x64"
-  region = "sfo3"
-  size   = "s-1vcpu-1gb-35gb-intel"
+  name     = "sige-api-homolog"
+  image    = "debian-11-x64"
+  region   = "sfo3"
+  size     = "s-1vcpu-1gb-35gb-intel"
+  ssh_keys = [digitalocean_ssh_key.sige_ssh_key.fingerprint]
+}
+
+resource "digitalocean_project" "lappis_energy" {
+  name        = "Lappis Energy Projects"
+  description = "Projects related to energy"
+  resources   = [digitalocean_droplet.api_homolog.urn]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,16 @@ variable "do_token" {
   description = "Digital Ocean Token"
   type        = string
 }
+
+variable "ssh_key" {
+  description = "SSH Key ID to access the Droplets"
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = "DigitalOcean region - San Francisco"
+  type        = string
+  default     = "sfo3"
+}
+
