af558207778a490a8406747ac24b19430b4a62f5
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-08-03 13:10:38 -0300

    chore: remove config terraform remote state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,18 +14,3 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-
-data "terraform_remote_state" "gl_remote_state" {
-  backend = "http"
-  config = {
-    username = var.gl_username
-    password = var.gl_token
-    address  = var.remote_state_address
-    lock_address = var.remote_lock_address
-    unlock_address = var.remote_unlock_address
-    lock_method = "POST"
-    unlock_method = "DELETE"
-    retry_wait_max = 3
-  }
-}
-
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,30 +20,3 @@ variable "host_path_file" {
   type        = string
   default     = "../ansible/hosts"
 }
-
-
-variable "gl_username" {
-  description = "Gitlab username to query remote state"
-  type        = string
-}
-
-variable "gl_token" {
-  description = "GitLab access token to query remote state"
-  type        = string
-}
-
-variable "remote_state_address" {
-  description = "Gitlab Address the state"
-  type        = string
-}
-
-variable "remote_lock_address" {
-  description = "Gitlab Address to lock the state"
-  type        = string
-}
-
-
-variable "remote_unlock_address" {
-  description = "Gitlab Address to unlock the state"
-  type        = string
-}
