d2f80f9d7f4e3782d3c572b9be878675e752618e
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-08-03 11:28:53 -0300

    chore: configure gitLab backend as a remote data source

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     digitalocean = {
@@ -5,8 +6,26 @@ terraform {
       version = "2.39.2"
     }
   }
-}
+  backend "http" {
+  }
 
+}
 provider "digitalocean" {
   token = var.do_token
 }
+
+
+data "terraform_remote_state" "gl_remote_state" {
+  backend = "http"
+  config = {
+    username = var.gl_username
+    password = var.gl_token
+    address  = var.remote_state_address
+    lock_address = var.remote_lock_address
+    unlock_address = var.remote_unlock_address
+    lock_method = "POST"
+    unlock_method = "DELETE"
+    retry_wait_max = 3
+  }
+}
+
